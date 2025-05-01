ba259cd01f3e31dcbe3227abd706ed93c165222a
Nicholas C Murray <murrnich@oregonstate.edu>
2025-05-01 14:21:00 -0700

    comment print statement

--- is_unique/main.py
+++ is_unique/main.py
@@ -23,14 +23,21 @@ def is_unique_no_hash(s: str) -> bool:
     """
     for i in range(len(s)):
         for j in range(i + 1, len(s)):
-            print("i: ", s[i], "j: ", s[j])
+            # print("i: ", s[i], "j: ", s[j])
             if s[i] == s[j]:
                 return False
     return True
 
 if __name__ == "__main__":
-    test_cases = ["abc", "aabbcc", "abca"]
+    test_cases = ["abc", "aabbcc", "abca", "abcdprstp"]
 
     for case in test_cases:
         print("hash: ", is_unique_hash(case))
         print("no_hash: ", is_unique_no_hash(case))
+
+#########
+# Hints:
+#########
+# try a hash table
+# Could a bit vector be useful?
+# Can you solve it in O(n log n) time? What might a solution like that look like
